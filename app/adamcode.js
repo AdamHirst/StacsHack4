f31e38beefac2cae56c2515831fb9fd35752fb29
Adam Hirst <adam@ahirst.com>
2018-04-07 18:47:08 +0100

    Geolocation

---
+++ app/adamcode.js
@@ -0,0 +1,47 @@
+// SkyScanner integration
+
+const apiBase = 'http://partners.api.skyscanner.net/apiservices';
+const request = require('request');
+const apiKey = process.env.SKYSCANNER_KEY;
+
+module.exports = {
+
+    getQuotes: (market, origin, destination, outboundDate, inboundDate, cb) => {
+        const url = `${apiBase}/browsequotes/v1.0/${market}/GBP/en-US/${origin}/${destination}/${outboundDate}/${inboundDate}?apiKey=${apiKey}`;
+
+        var options = {
+            url: url,
+            headers: {
+                'Accept': 'application/json'
+            }
+        };
+
+        request(options, function(err, response, body) {
+            if (err) return cb(err);
+
+            cb(null, JSON.parse(body).Quotes);
+        });
+    },
+
+    getGeolocatedString: (country, query, cb) => {
+        const url = `${apiBase}/autosuggest/v1.0/${country}/GBP/en-US?query=${query}&apiKey=${apiKey}`;
+
+        var options = {
+            url: url,
+            headers: {
+                'Accept': 'application/json'
+            }
+        };
+
+        request(options, function(err, response, body) {
+            if (err) return cb(err);
+
+            if (!body.Places[0].PlaceId) {
+                cb(new Error("No place for string"));
+            }
+
+            cb(null, body.Places[0].PlaceId);
+        });
+    }
+    
+}
--- app/routes.js
+++ app/routes.js
@@ -1,21 +1,74 @@
 // Routes - Project routes
 var ben = require("./bencode.js")
-var hacks = []
-ben.scrape(doneScraping);
+var hacks = [];
+var needsUpdate = true;
 
-function doneScraping(h) {
-	hacks = h;
+const updateHackathons = (cb) => {
+    needsUpdate = false;
+
+    ben.scrape(hackathons => {
+        hacks = hackathons;
+        
+        setTimeout(() => {
+            needsUpdate = true;
+        }, 1000 * 60 * 60);
+
+        cb();
+    });
 }
+
+const getHackathons = (cb) => {
+    if (needsUpdate) updateHackathons(() => {
+        cb(hacks);
+    }) 
+    else {
+        cb(hacks);
+    }
+}
+
+const adam = require('./adamcode.js');
 
 module.exports = (app) => {
 
     // Index page
     app.get('/', (req, res) => {
-        res.render('index', { hackathons: hacks })
+        getHackathons(hackathons => {
+            res.render('index', { hackathons: hackathons });
+        });
     });
 
     app.get('/hackathons', (req, res) => {
-        res.send({ hackathons: hacks });
+        getHackathons(hackathons => {
+            res.send({ hackathons: hackathons });
+        });
     });
+
+    app.get('/getprice', (req, res) => {
+        if (!req.query.market) {
+            res.send("Error! A market is required");
+        } else if (!req.query.origin) {
+            res.send("Error! An origin is required");
+        } else if (!req.query.destination) {
+            res.send("Error! A destination is required");
+        } else if (!req.query.outboundDate) {
+            res.send("Error! An outbound date is required");
+        } else if (!req.query.inboundDate) {
+            res.send("Error! An inbound date is required");
+        } else {
+            adam.getQuotes(req.query.market, req.query.origin, req.query.destination, req.query.outboundDate, req.query.inboundDate, (err, quotes) => {
+                if (err) res.send(err);
+                else {
+                    res.send(quotes);
+                }
+            });
+        }
+    });
+
+    app.get('/equivlocale', (req, res) => {
+        adam.getGeolocatedString(req.query.country, req.query.query, (err, geoString) => {
+            if (err) return res.send(err);
+            res.send(geoString);
+        });
+    })
 
 }
