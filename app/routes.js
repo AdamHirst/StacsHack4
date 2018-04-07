555b45dbbd70b4add5be4cacdfb6a7b439fd0319
Adam Hirst <adam@ahirst.com>
2018-04-07 14:32:33 +0100

    Added template

---
+++ app/routes.js
@@ -0,0 +1,15 @@
+// Routes - Project routes
+
+module.exports = (app) => {
+
+    // Index page
+    app.get('/', (req, res) => {
+        res.send('Hello world!');
+    });
+
+    app.get('/hackathons', (req, res) => {
+        res.send({ hackathons: [] });
+    });
+
+
+}
